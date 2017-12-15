4a4d4f6421f02d6a69f8e3f267a25c5fcfc3c17f
Skyler Layne <skylerclayne@gmail.com>
2017-12-15 11:52:53 -0500

    Test for chef automate setup
    Signed-off-by: Skyler Layne <skylerclayne@gmail.com>

--- spec/automate_soup_spec.rb
+++ spec/automate_soup_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe AutomateSoup do
   it 'has a version number' do
     expect(AutomateSoup::VERSION).not_to be nil
   end
+
+  it 'creates a new instance of AutomateSoup with password' do
+    soup = AutomateSoup.setup(
+      url: 'test',
+      username: 'test',
+      password: 'test'
+    )
+    expect(soup).not_to be nil
+  end
+
+  it 'creates a new instance of AutomateSoup with token' do
+    soup = AutomateSoup.setup(
+      url: 'test',
+      username: 'test',
+      token: 'test'
+    )
+    expect(soup).not_to be nil
+  end
 end
